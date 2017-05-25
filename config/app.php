82bce413bf59847df7ab3d8d80a23ee14e8be0ac
OEM Configuration (temporary user) <webdesign.moises@gmail.com>
2017-05-25 11:06:19 -0300

    mudando permissões

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Miss Daisy',
+    'name' => 'Loja Laravel',
 
     /*
     |--------------------------------------------------------------------------
